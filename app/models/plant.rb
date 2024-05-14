7a15aafed353149134e5b0e7583cd0e000837cdd
Tobias Klöpper <tobiaskloepper1@web.de>
2024-05-14 06:59:18 +0200

    added time formatting to the index page

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -4,4 +4,13 @@ class Plant < ApplicationRecord
     validates :last_watering, presence: true
 
     has_many_attached :images
+
+    def age
+        age = DateTime.now - DateTime.parse("#{birthday}")
+        age.to_i
+    end
+
+    def birth
+        self.birthday.strftime("%Y/%m/%d")
+    end
 end
