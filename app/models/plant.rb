3a4815b5f05bb27caa7c004f65dd846752093efb
Tobias Klöpper <tobiaskloepper1@web.de>
2024-06-03 16:49:24 +0200

    removed validation for the old watering model during model creation

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,6 @@
 class Plant < ApplicationRecord
     validates :name, presence: true
     validates :birthday, presence: true
-    validates :last_watering, presence: true
 
     has_many_attached :images
     has_many :waterings
